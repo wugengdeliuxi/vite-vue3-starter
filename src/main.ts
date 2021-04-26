beb5b26cf5ea8930ac85b62cbcd831d7316b2a59
黄杰 <huangjie@iotsh.com.cn>
2021-04-26 15:47:00 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import App from './App.vue'
+
+import router from './router/index'
+import store from './store/index'
+
+import 'element-plus/lib/theme-chalk/index.css'
+
+createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
